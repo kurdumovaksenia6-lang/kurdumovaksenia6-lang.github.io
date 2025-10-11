456a4a15d04ba3ebf15c822f02d80b4044ec0737
kurdumovaksenia6-lang <kurdumovaksenia6@gmail.com>
2025-10-11 13:47:54 +0700

    javascript style alert

---
+++ color.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function()
+    {let body = document.querySelector("body");
+document.querySelector("#red").addEventListener("click",
+            function(){ body.style.backgroundColor = "red";}
+        );
+document.querySelector("#green").addEventListener("click",
+        function(){ body.style.backgroundColor = "green";}
+        );
+document.querySelector("#blue").addEventListener("click",
+        function(){ body.style.backgroundColor = "blue";}
+        );
+document.querySelector("#purple").addEventListener("click",
+            function(){ body.style.backgroundColor = "purple";}
+        );
+document.querySelector("#yellow").addEventListener("click",
+            function(){ body.style.backgroundColor = "yellow";}
+        );
+document.querySelector("#orange").addEventListener("click",
+            function(){ body.style.backgroundColor = "orange";}
+        );
+    })
